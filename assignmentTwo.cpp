fc812e7ba88d6f3fb58c8afe8f309f28914e2ada
SkapeBreak <oriskatebreakginal@gmail.com>
2024-10-09 14:51:45 -0600

    Fixed synthax

--- assignmentTwo.cpp
+++ assignmentTwo.cpp
@@ -1,27 +1,28 @@
-#include (iostream)
+using namespace std;
+#include <iostream>
 #include <cmath>
-using namespace std
 
 const float PI = 3.14159f;
 
 float DegreesToRadians(float degrees)
 {
-  degrees * PI / 180.0f;
+  return degrees * PI / 180.0f;
+}
 
-
-float CalcTriangleEdge(float a float b float gamma)
-  return sqrt(a * a + b * b - 2 * a * b * cos gamma);
+float CalcTriangleEdge(float a, float b, float gamma)
+{
+  return sqrt(a * a + b * b - 2 * a * b * cos(DegreesToRadians(gamma)));
 }
 
 int main()
 {
-  auto a, b, gamma;
-  cout << "Enter the length side a: ";
-  cin << a;
-  cout << "Enter the length of side b: "
-  cin << b;
+  float a, b, gamma;
+  cout<< "Enter the length side a: " << endl;
+  cin >> a;
+  cout << "Enter the length of side b: ";
+  cin >> b;
   cout << "Enter the angle gamma: ";
-  cin << gmma;
-  cout << "The length of side c is " << CalcTriangleEdge(a, b, RadiansToDegrees(gamma)) << end;
-  return "0";
+  cin >> gamma;
+  cout << "The length of side c is: " << CalcTriangleEdge(a, b, gamma) << endl;
+  return 0;
 }
